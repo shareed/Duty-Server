855e844343f423806e7281579398001611810c29
shareed <shanreed6@gmail.com>
2020-07-31 17:58:40 -0700

    added basic express server

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const server = require('./api/server.js');
 
 
 server.get('/', (req, res) => {
-    const message = process.env.EVNM;
-    res.send(message)
+    // const message = process.env.EVNM;
+    // res.send(message)
+    res.json({GREETING: "This is the api for Duty created By Shannon Reed:"})
+})
+
+server.get('/duty/users', (req, res) => {
+    res.json({WELCOME:"YOU HAVE HIT THE USERS ENDPOINT"})
 })
 
 const port = process.env.PORT;
